51455472b5a3dbb9786814de58369a808500d9a7
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-25 17:08:33 -0300

    [NEW] Analise da resposta
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ src/erpbrasil/edoc/resposta.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 - TODAY Luis Felipe Mileo - KMEE INFORMATICA LTDA
+# License MIT
+
+import re
+from lxml import etree
+
+
+class RetornoSoap(object):
+
+    def __init__(self, webservice, raiz, xml, retorno, resposta):
+        self.webservice = webservice
+        self.envio_raiz = raiz
+        self.envio_xml = xml
+        self.resposta = resposta
+        self.retorno = retorno
+
+
+def analisar_retorno(operacao, raiz, xml, retorno, classe):
+    retorno.raise_for_status()
+    match = re.search('<soap:Body>(.*?)</soap:Body>', retorno.text)
+    if match:
+        xml_resposta = match.group(1)
+        resultado = etree.tostring(etree.fromstring(xml_resposta)[0])
+        classe.Validate_simpletypes_ = False
+        resposta = classe.parseString(resultado.encode('utf-8'), silence=True)
+        return RetornoSoap(operacao, raiz, xml, retorno, resposta)
