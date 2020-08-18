0adcc98471e0d833d7104fd25f7cb3debec69c67
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-08-18 13:34:33 -0300

    [FIX] erpbrasil.edoc.nfse.issnet: Correção da URL de Produção

--- src/erpbrasil/edoc/nfse/cidades.py
+++ src/erpbrasil/edoc/nfse/cidades.py
@@ -4,16 +4,18 @@
 from __future__ import division, print_function, unicode_literals
 
 from erpbrasil.edoc.nfse.ginfes import Ginfes
-# from erpbrasil.edoc.nfse.dsf import Dsf
+from erpbrasil.edoc.nfse.dsf import Dsf
+from erpbrasil.edoc.nfse.issnet import Issnet
 
 cidades = {
-    # 1501402: Dsf,  # Belem-PA
-    # 2211001: Dsf,  # Teresina - PI
-    3132404: Ginfes,  # ITAJUBA - MG
-#     3170206: Dsf,  # Uberlândia-MG
-#     3303500: Dsf,  # Nova Iguaçu - RJ
-#     3509502: Dsf,  # Campinas - SP
-#     5002704: Dsf,  # Campo Grande - MS
+    1501402: Dsf,       # Belem-PA
+    2211001: Dsf,       # Teresina - PI
+    3132404: Ginfes,    # ITAJUBA - MG
+    3543402: Issnet,    # Ribeirão Preto - SP
+    3170206: Dsf,       # Uberlândia-MG
+    3303500: Dsf,       # Nova Iguaçu - RJ
+    3509502: Dsf,       # Campinas - SP
+    5002704: Dsf,       # Campo Grande - MS
 }
 
 
--- src/erpbrasil/edoc/nfse/issnet.py
+++ src/erpbrasil/edoc/nfse/issnet.py
@@ -2,7 +2,10 @@
 # Copyright (C) 2020 - TODAY, Marcel Savegnago - Escodoo
 
 from __future__ import division, print_function, unicode_literals
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
+from erpbrasil.base import misc
 from erpbrasil.edoc.nfse import NFSe, ServicoNFSe
 
 from nfselib.issnet.v1_00 import servico_consultar_situacao_lote_rps_envio as consulta_situacao_lote
@@ -17,7 +20,13 @@ from nfselib.issnet.v1_00.tipos_complexos import tcIdentificacaoNfse, tcIdentifi
 #from nfselib.issnet.v1_00.Servico_Cancelar_justificativa_tipo_complexos import tcPedidoCancelamento, tcInfPedidoCancelamento
 
 
-endpoint = 'webserviceabrasf/homologacao/servicos.asmx?WSDL'
+cidade = {
+
+    3543402: 'ribeiraopreto', # Ribeirão Preto - SP
+
+}
+
+endpoint = 'servicos.asmx?WSDL'
 
 servicos = {
     'envia_documento': ServicoNFSe(
@@ -43,9 +52,9 @@ class Issnet(NFSe):
                  im_prestador):
 
         if ambiente == '2':
-            self._url = 'http://www.issnetonline.com.br'
+           self._url = 'https://www.issnetonline.com.br/webserviceabrasf/homologacao/'
         else:
-            self._url = 'http://www.issnetonline.com.br'
+            self._url = 'https://www.issnetonline.com.br/webserviceabrasf/' + cidade[int(cidade_ibge)] + '/'
         self._servicos = servicos
 
         super(Issnet, self).__init__(
@@ -114,25 +123,7 @@ class Issnet(NFSe):
             return True
         return False
 
-    def _prepara_cancelar_nfse_envio(self, doc_numero, justificativa):
-        # raiz = servico_cancelar_nfse_envio.CancelarNfseEnvio(
-        #     Pedido=tcPedidoCancelamento(
-        #         InfPedidoCancelamento=tcInfPedidoCancelamento(
-        #             id=doc_numero,
-        #             IdentificacaoNfse=tcIdentificacaoNfse(
-        #                 Numero=doc_numero,
-        #                 CpfCnpj=tcCpfCnpj(
-        #                     Cnpj=self.cnpj_prestador,
-        #                 ),
-        #                 InscricaoMunicipal=self.im_prestador,
-        #                 CodigoMunicipio=self.cidade
-        #             ),
-        #             CodigoCancelamento='5',
-        #             MotivoCancelamentoNfse='Cancelado pelo motivo xyz - ISSNET / Nota Control',
-        #         )
-        #     )
-        # )
-
+    def _prepara_cancelar_nfse_envio(self, doc_numero):
         Pedido = tcPedidoCancelamento(
             InfPedidoCancelamento=tcInfPedidoCancelamento(
                 id=doc_numero,
@@ -149,8 +140,6 @@ class Issnet(NFSe):
 
         # Foi codificado desta forma porque a assinatura fica dentro da tag Pedido. Acredito que de para melhorar.
         pedido = self.assina_raiz(Pedido, "")
-        # xml_string, xml_etree = self._generateds_to_string_etree(pedido)
-
         xml_assinado = '<?xml version="1.0"?>' \
                        '<p1:CancelarNfseEnvio ' \
                        'xmlns:p1="http://www.issnetonline.com.br/webserviceabrasf/vsd/servico_cancelar_nfse_envio.xsd" ' \
@@ -168,36 +157,84 @@ class Issnet(NFSe):
     #             Tipo=rps_tipo,
     #         ),
     #         Prestador=tcIdentificacaoPrestador(
-    #             CpfCnpj=tcCpfCnpj(
-    #                 Cnpj=self.cnpj_prestador,
-    #             ),
+    #             Cnpj=self.cnpj_prestador,
     #             InscricaoMunicipal=self.im_prestador
     #         ),
     #     )
     #     xml_assinado = self.assina_raiz(raiz, '')
-    #     xml_assinado = '<?xml version="1.0"?>' + xml_assinado
     #
     #     return xml_assinado
-
-
-
-    # def processar_documento(self, edoc):
-    #     processo = super(NFSe, self).processar_documento(edoc)
     #
-    #     ultimo_processo = None
-    #     for p in processo:
-    #         ultimo_processo = p
+    # def analisa_retorno_consulta(self, processo, number, company_cnpj_cpf,
+    #                     company_legal_name):
+    #     retorno = ET.fromstring(processo.retorno)
+    #     nsmap = {'consulta': 'http://www.ginfes.com.br/servico_consultar_'
+    #                          'nfse_rps_resposta_v03.xsd',
+    #              'tipo': 'http://www.ginfes.com.br/tipos_v03.xsd'}
     #
-    #     if ultimo_processo.webservice == u'ConsultarSituacaoLoteRpsV3':
-    #         if processo.resposta.Situacao == 1:
-    #             print('Não Recebido')
+    #     mensagem = ''
+    #     if processo.webservice == 'ConsultarNfsePorRpsV3':
+    #         enviado = retorno.findall(
+    #             ".//consulta:CompNfse", namespaces=nsmap)
+    #         nao_encontrado = retorno.findall(
+    #             ".//tipo:MensagemRetorno", namespaces=nsmap)
     #
-    #         elif ultimo_processo.resposta.Situacao == 2:
-    #             print('Lote ainda não processado')
+    #         if enviado:
+    #             # NFS-e já foi enviada
     #
-    #         elif ultimo_processo.resposta.Situacao == 3:
-    #             print('Procesado com Erro')
+    #             cancelada = retorno.findall(
+    #                 ".//tipo:NfseCancelamento", namespaces=nsmap)
     #
-    #         elif ultimo_processo.resposta.Situacao == 4:
-    #             print('Procesado com Sucesso')
+    #             if cancelada:
+    #                 # NFS-e enviada foi cancelada
     #
+    #                 data = retorno.findall(
+    #                     ".//tipo:DataHora", namespaces=nsmap)[0].text
+    #                 data = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S'). \
+    #                     strftime("%m/%d/%Y")
+    #                 mensagem = 'NFS-e cancelada em ' + data
+    #
+    #             else:
+    #                 numero_retorno = \
+    #                     retorno.findall(".//tipo:InfNfse/tipo:Numero",
+    #                                     namespaces=nsmap)[0].text
+    #                 cnpj_prestador_retorno = retorno.findall(
+    #                     ".//tipo:IdentificacaoPrestador/tipo:Cnpj",
+    #                     namespaces=nsmap)[0].text
+    #                 razao_social_prestador_retorno = retorno.findall(
+    #                     ".//tipo:PrestadorServico/tipo:RazaoSocial",
+    #                     namespaces=nsmap)[0].text
+    #
+    #                 varibles_error = []
+    #
+    #                 if numero_retorno != number:
+    #                     varibles_error.append('Número')
+    #                 if cnpj_prestador_retorno != misc.punctuation_rm(
+    #                     company_cnpj_cpf):
+    #                     varibles_error.append('CNPJ do prestador')
+    #                 if razao_social_prestador_retorno != company_legal_name:
+    #                     varibles_error.append('Razão Social de pestrador')
+    #
+    #                 if varibles_error:
+    #                     mensagem = 'Os seguintes campos não condizem com' \
+    #                                ' o provedor NFS-e: \n'
+    #                     mensagem += '\n'.join(varibles_error)
+    #                 else:
+    #                     mensagem = "NFS-e enviada e corresponde com o provedor"
+    #
+    #         elif nao_encontrado:
+    #             # NFS-e não foi enviada
+    #
+    #             mensagem_erro = retorno.findall(
+    #                 ".//tipo:Mensagem", namespaces=nsmap)[0].text
+    #             correcao = retorno.findall(
+    #                 ".//tipo:Correcao", namespaces=nsmap)[0].text
+    #             codigo = retorno.findall(
+    #                 ".//tipo:Codigo", namespaces=nsmap)[0].text
+    #             mensagem = (codigo + ' - ' + mensagem_erro + ' - Correção: ' +
+    #                         correcao + '\n')
+    #
+    #         else:
+    #             mensagem = 'Erro desconhecido.'
+    #
+    #     return mensagem
