1cd601817dcfbcef8e73b0081ea6fe57cb642f21
Luis Malta <luis.malta@kmee.com.br>
2020-10-14 11:32:07 -0300

    [FIX] Temporarily comments DSF

--- src/erpbrasil/edoc/nfse/cidades.py
+++ src/erpbrasil/edoc/nfse/cidades.py
@@ -4,16 +4,16 @@
 from __future__ import division, print_function, unicode_literals
 
 from erpbrasil.edoc.nfse.ginfes import Ginfes
-from erpbrasil.edoc.nfse.dsf import Dsf
+# from erpbrasil.edoc.nfse.dsf import Dsf
 
 cidades = {
-    1501402: Dsf,  # Belem-PA
-    2211001: Dsf,  # Teresina - PI
+    # 1501402: Dsf,  # Belem-PA
+    # 2211001: Dsf,  # Teresina - PI
     3132404: Ginfes,  # ITAJUBA - MG
-    3170206: Dsf,  # Uberlândia-MG
-    3303500: Dsf,  # Nova Iguaçu - RJ
-    3509502: Dsf,  # Campinas - SP
-    5002704: Dsf,  # Campo Grande - MS
+#     3170206: Dsf,  # Uberlândia-MG
+#     3303500: Dsf,  # Nova Iguaçu - RJ
+#     3509502: Dsf,  # Campinas - SP
+#     5002704: Dsf,  # Campo Grande - MS
 }
 
 
