edd7588eb8d2b88ec56546236d350b22dd71b85d
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-11-23 23:50:39 -0300

    [FIX] regornizando arquivo cidades.py

--- src/erpbrasil/edoc/nfse/cidades.py
+++ src/erpbrasil/edoc/nfse/cidades.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 # Copyright (C) 2019  Luis Felipe Mileo - KMEE
 
-from __future__ import division, print_function, unicode_literals
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 from erpbrasil.edoc.nfse.ginfes import Ginfes
 from erpbrasil.edoc.nfse.dsf import Dsf
@@ -11,11 +13,11 @@ cidades = {
     1501402: Dsf,       # Belem-PA
     2211001: Dsf,       # Teresina - PI
     3132404: Ginfes,    # ITAJUBA - MG
-    3543402: Issnet,    # Ribeirão Preto - SP
     3170206: Dsf,       # Uberlândia-MG
     3303500: Dsf,       # Nova Iguaçu - RJ
     3509502: Dsf,       # Campinas - SP
     5002704: Dsf,       # Campo Grande - MS
+    3543402: Issnet,    # Ribeirão Preto - SP
 }
 
 
