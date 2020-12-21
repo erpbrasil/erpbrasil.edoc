483e53b22444940b048a4a9da855e6f0dd22e9ce
Luis Felipe Mileo <mileo@kmee.com.br>
2020-12-21 09:53:02 -0300

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def read(*names, **kwargs):
 
 setup(
     name='erpbrasil.edoc',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Emissão de documentos fiscais e outras obrigações (NF-E, NFS-E, MDF-E, CT-E, REINF, E-SOCIAL)',
     long_description='%s\n%s' % (
--- src/erpbrasil/edoc/__init__.py
+++ src/erpbrasil/edoc/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
     from pkgutil import extend_path
 
     __path__ = extend_path(__path__, __name__)
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 import abc
 
